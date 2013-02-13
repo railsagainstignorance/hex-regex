ff2df2a2cab8dfeaedf78410df62e16327bf7740
Chris <cgatherc@hotmail.com>
2013-02-13 21:58:09 +0000

    adding error checking to full_hex for equal sides, added default pattern to regexs

--- full_hex.rb
+++ full_hex.rb
@@ -20,6 +20,14 @@ class Full_hex
   def all_inputs_validated?
     @input_woes = []
     @input_woes << "Full_hex does not have 3 sides, it has #{@sides.length}." if @sides.length != 3
+
+    rows_in_first_side = @sides.first.row_regexs.length
+    rows_in_second_side = @sides[1].row_regexs.length
+    rows_in_third_side = @sides[2].row_regexs.length
+
+    @input_woes << "side 1 has #{rows_in_first_side} rows, but side 2 has #{rows_in_second_side} rows." if rows_in_second_side != rows_in_first_side
+    @input_woes << "side 1 has #{rows_in_first_side} rows, but side 3 has #{rows_in_third_side} rows." if rows_in_third_side != rows_in_first_side
+
     return @input_woes.length == 0
   end
 
--- row_regex.rb
+++ row_regex.rb
@@ -1,14 +1,16 @@
 class Row_regex
-  def initialize( pattern )
+  @@DEFAULT_PATTERN = '.*'
+
+  def initialize( pattern = @@DEFAULT_PATTERN )
     @full = Regexp.new( '^' + pattern + '$' )
     @partials = []
   end
 
-  def add_partial_left(pattern)
+  def add_partial_left(pattern = @@DEFAULT_PATTERN)
     @partials << Regexp.new( '^' + pattern )
   end
 
-  def add_partial_right(pattern)
+  def add_partial_right(pattern = @@DEFAULT_PATTERN)
     @partials << Regexp.new( pattern + '$' )
   end
 
