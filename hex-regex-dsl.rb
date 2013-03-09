182a47870f4fd496098e93ad2b9c7bfaa89f3d94
Chris <cgatherc@hotmail.com>
2013-03-09 00:47:19 +0000

    added error checks

--- hex-regex-dsl.rb
+++ hex-regex-dsl.rb
@@ -6,19 +6,28 @@ require File.dirname(__FILE__) + '/full_hex.rb'
 # - partial_left
 # - partial_right
 
+def message_and_exit(text)
+  puts 'Error: ' + text
+  puts 'at ' + caller[1].split(':')[0..1].join(', line ')
+  exit
+end
+
 def side()
   Full_hex.instance.add_side Side.new()
 end
 
 def full_row(text)
+  message_and_exit "no side exists for row: \"#{text}\"" if Full_hex.instance.last_side.nil?
   Full_hex.instance.last_side.add_row Row_regex.new(text)
 end
 
 def partial_left(text)
+  message_and_exit "no row exists for partial_left: \"#{text}\"" if Full_hex.instance.last_side.nil? or Full_hex.instance.last_side.last_row.nil?
   Full_hex.instance.last_side.last_row.add_partial_left( text )
 end
 
 def partial_right(text)
+  message_and_exit "no row exists for partial_right: \"#{text}\"" if Full_hex.instance.last_side.nil? or Full_hex.instance.last_side.last_row.nil?
   Full_hex.instance.last_side.last_row.add_partial_right( text )
 end
 
